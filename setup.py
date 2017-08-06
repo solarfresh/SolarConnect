f2f3c8878e2823c41b3603f3ace36e9363173564
SolarFresh <shangyuhuang@gmail.com>
2017-08-07 01:23:46 +0800

    mysql-connector should be installed from OS

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ setup(
     install_requires=[
         "google-api-python-client>=1.5.5",
         "maxminddb>=1.3.0",
-        "mysql-connector>=2.0.4",
+        # should be installed from OS
+        #"mysql-connector>=2.0.4",
         "oauth2client>=3.0.0",
         "pandas>=0.20.3",
         "PyHive>=0.2.1",
